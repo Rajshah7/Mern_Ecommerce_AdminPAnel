e11c3a1b426f2ab19b2da7ee5e7eb06083d65cbb
Rajshah7 <rajshah97120@gmail.com>
2022-03-12 15:02:58 +0530

    This is first commit

---
+++ src/containers/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Layout from '../../components/Layout';
+import './style.css';
+import { NavLink } from 'react-router-dom'
+
+/**
+* @author
+* @function Home
+**/
+
+const Home = (props) => {
+    return (
+        <Layout sidebar>
+
+            {/* <div style={{ margin: '5rem' }} className="text-center">
+                <h1>Welcome to Admin Dashboard</h1>
+            </div>*/}
+        </Layout>
+    )
+
+}
+
+export default Home
